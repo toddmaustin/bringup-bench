4132ca5dd66f728dcc99fb4c5647828a886eab79
Todd Austin <austin@agitalabs.com>
2023-04-16 19:45:46 -0400

    More bringup-bench build debug.

--- frac-calc/frac-calc.c
+++ frac-calc/frac-calc.c
@@ -105,7 +105,6 @@ MFILE *minput = &input;
 #if DEBUG==0
 #define NDEBUG
 #endif
-/* #include <assert.h> */ /* Verify assumptions with assert. Turn off with #define NDEBUG */ 
 
 /** @brief Debug message if DEBUG on */
 #define IFDEBUG(M) if(DEBUG) libmin_printf("[DEBUG file:%s line:%d]: " M "\n", __FILE__, __LINE__); else {;}
