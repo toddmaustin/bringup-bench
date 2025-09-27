ade6b67081f8c9b8488d0234d9ab9a00af40cd76
Todd Austin <austin@umich.edu>
2025-09-27 07:27:07 -0400

    First Mojo-V hand-built test program!

--- Makefile
+++ Makefile
@@ -165,6 +165,19 @@ TARGET_CLEAN =
 TARGET_EXCLUDES =
 TARGET_CONFIGURED = 1
 TARGET_REFEXT = out
+else ifeq ($(TARGET), mojo-v)
+TARGET_CC = riscv64-unknown-elf-gcc
+#TARGET_CC = riscv64-unknown-elf-clang
+TARGET_AR = riscv64-unknown-elf-ar
+TARGET_CFLAGS = -DTARGET_SPIKE -DLIBMIN_MALLOC_ALIGN_BYTES=8 -march=rv64gczicond -mabi=lp64d -static -mcmodel=medlow -Wall -fvisibility=hidden -nostdlib -nostartfiles -ffreestanding -ffixed-t3 -ffixed-t4 -ffixed-t5 -ffixed-t6 # -MMD -mcmodel=medany
+TARGET_LIBS = -lgcc
+TARGET_SIM = ../../riscv-isa-sim/build/spike --isa=rv64gczicond --extlib=../target/spike_mmio_plugin.so -m0x100000:0x820000 --device=spike_mmio_plugin,0x20000
+TARGET_DIFF = diff
+TARGET_EXE = $(PROG).elf
+TARGET_CLEAN = *.d ibex_simple_system_pcount.csv
+TARGET_EXCLUDES = anagram c-interp checkers lz-compress rsa-cipher spelt2num
+TARGET_CONFIGURED = 1
+TARGET_REFEXT = out
 else
 # default is an unconfigured
 TARGET_CONFIGURED = 0
@@ -218,6 +231,8 @@ else ifeq ($(TARGET), spike32-pk)
 	$(TARGET_CC) $(CFLAGS) -o $@ $^ $(LIBS) $(TARGET_LIBS)
 else ifeq ($(TARGET), spike64-pk)
 	$(TARGET_CC) $(CFLAGS) -o $@ $^ $(LIBS) $(TARGET_LIBS)
+else ifeq ($(TARGET), mojo-v)
+	$(TARGET_CC) $(CFLAGS) -T ../target/spike-map.ld $^ ../target/spike-crt0.S -o $@ $(LIBS) $(TARGET_LIBS)
 else
 	$(error MODE is not defined (add: TARGET={host|sa}).)
 endif
---
+++ mojo-test/mojo-test.c
@@ -0,0 +1,82 @@
+#include "libmin.h"
+
+// Define your custom CSR number
+#define CSR_MPRIVREGCFG 0x0a0
+
+// Inline helpers
+static inline uint64_t
+read_mprivregcfg(void)
+{
+  uint64_t value;
+  asm volatile ("csrr %0, %1" : "=r"(value) : "i"(CSR_MPRIVREGCFG));
+  return value;
+}
+
+static inline void
+write_mprivregcfg(uint64_t value)
+{
+  asm volatile ("csrw %0, %1" :: "i"(CSR_MPRIVREGCFG), "rK"(value));
+}
+
+// Predefined memory values
+volatile uint64_t x = 15;
+volatile uint64_t max = 25;
+
+int
+ main(void)
+{
+  //
+  // mprivregcfg tests
+  //
+  libmin_printf("** Running CSR[privreg] tests...\n");
+
+  uint64_t val;
+
+  // read reset value
+  val = read_mprivregcfg();
+  libmin_printf("Initial mprivregcfg = 0x%lx\n", val);
+
+  // enable private register semantics (bit 0 = 1)
+  write_mprivregcfg(1);
+
+  val = read_mprivregcfg();
+  libmin_printf("After enable, mprivregcfg = 0x%lx\n", val);
+
+
+  // do some secret computation
+  libmin_printf("Initial inputs: x:%lu, max:%lu\n", x, max);
+
+  // inline assembly block
+  asm volatile (
+    // load third-party encrypted operands
+    "ld        /*p0*/t3, (%0)\n\t"   // p0 = x
+    "ld        /*p1*/t4, (%1)\n\t"   // p1 = max
+
+    // Condition: (max < x)?
+    "slt       /*p2*/t5, /*p1*/t4, /*p0*/t3\n\t" // p2 = (p1 < p0) ? 1 : 0
+
+    // Build data-oblivious conditional result
+    "czero.eqz /*p0*/t3, /*p0*/t3, /*p2*/t5\n\t" // if p2==0 => p0=0, else p0=x
+    "czero.nez /*p1*/t4, /*p1*/t4, /*p2*/t5\n\t" // if p2!=0 => p1=0, else p1=max
+    "or        /*p3*/t6, /*p0*/t3, /*p1*/t4\n\t" // select: p3 = (x if x>max else max)
+
+    // Store third-party encrypted (potentially) new max value
+    "sw     /*p3*/t6, (%1)\n\t"
+
+    :
+    : "r" (&x), "r" (&max)   // input operands
+    : "t3", "t4", "t5", "t6" // clobbered registers
+  );
+
+  libmin_printf("Final results:   x:%lu, max:%lu\n", x, max);
+
+
+  // disable private register semantics (write 0)
+  write_mprivregcfg(0);
+
+  val = read_mprivregcfg();
+  libmin_printf("After disable, mprivregcfg = 0x%lx\n", val);
+
+  libmin_success();
+}
+
